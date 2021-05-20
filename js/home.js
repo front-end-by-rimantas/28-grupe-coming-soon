8de8862ad6c7939fffbd33862a93fcdb34967e19
Rimantas <belauzas@yahoo.com>
2021-05-20 21:10:13 +0300

    Laiko formatavimas

--- js/home.js
+++ js/home.js
@@ -1,9 +1,10 @@
 // IMPORT
 import { Clock } from "./components/clock/Clock.js";
 import { socials } from "./components/socials/socials.js";
+import { clockData } from "./data/clockData.js";
 import { socialsData } from "./data/socialsData.js";
 
 // EXECUTION
-new Clock('#clock_1');
+new Clock('#clock_1', clockData);
 // new Clock(485545);
 socials('footer .socials', socialsData);
