65352b0d63e8a058a8b3ab95e7608571945e5911
Rimantas <belauzas@yahoo.com>
2021-06-26 13:56:31 +0300

    Basic form validation

--- js/components/form/Form.js
+++ js/components/form/Form.js
@@ -2,7 +2,7 @@ class Form {
     constructor(selector) {
         this.selector = selector;
 
-        this.DOM = null;
+        this.formDOM = null;
         this.allInputsDOM = [];
         this.submitButtonDOM = null;
 
@@ -10,27 +10,84 @@ class Form {
     }
 
     init() {
-        // patikrinti, ar validus selector
-        // jei ne, baigiam darba
+        if (!this.isValidSelector()) {
+            console.error('ERROR: nevalidus selector');
+            return false;
+        }
 
-        // susirasti DOM elementa
-        // jei rasti nepavyksta, baigiam darba
+        this.formDOM = document.querySelector(this.selector);
+        if (!this.formDOM) {
+            console.error('ERROR: nerastas formos elementas');
+            return false;
+        }
 
-        // susirasti visus formos laukus: input, textarea
-        // susirasti formos submit mygtuka
+        this.allInputsDOM = this.formDOM.querySelectorAll('input, textarea');
+        this.submitButtonDOM = this.formDOM.querySelector('button[type="submit"]');
 
-        // uzregistruojame mygtuko paspaudimo ivyki
         this.addEvents();
     }
 
+    isValidSelector() {
+        return true;
+    }
+
+    isValidName(name) {
+        if (this.isEmptyString(name)) {
+            return false;
+        }
+        return true;
+    }
+
+    isValidEmail(email) {
+        if (this.isEmptyString(email)) {
+            return false;
+        }
+        return true;
+    }
+
+    isValidText(text) {
+        if (this.isEmptyString(text)) {
+            return false;
+        }
+        return true;
+    }
+
+    isEmptyString(string) {
+        return typeof string !== 'string' || string === '';
+    }
+
     addEvents() {
-        // submit mygtuko paspaudimo metu reikia isjungti default veikima
+        this.submitButtonDOM.addEventListener('click', (event) => {
+            // submit mygtuko paspaudimo metu reikia isjungti default veikima
+            event.preventDefault();
 
-        // issitraukti is visu formos lauku informacija
-        // eiti per visus laukus ir atpazinus informacijos tipa atlikti tos informacijos validacija
+            // issitraukti is visu formos lauku informacija
+            // eiti per visus laukus ir atpazinus informacijos tipa atlikti tos informacijos validacija
+            let allGood = true;
 
-        // jei patikrinus visus laukus, nerasta nei vienos klaidos, tai "siunciam pranesima"
-        // jei patikrinus visus laukus, nerasta bent viena klaida, tai parodome visu klaidos pranesimus (kol kas, viskas pateikiama i console)
+            for (const inputDOM of this.allInputsDOM) {
+                const validationRule = inputDOM.dataset.validation;
+                const value = inputDOM.value;
+
+                if (validationRule === 'email' && !this.isValidEmail(value)) {
+                    allGood = false;
+                    break;
+                }
+                if (validationRule === 'name' && !this.isValidName(value)) {
+                    allGood = false;
+                    break;
+                }
+                if (validationRule === 'text' && !this.isValidText(value)) {
+                    allGood = false;
+                    break;
+                }
+            }
+
+
+            // jei patikrinus visus laukus, nerasta nei vienos klaidos, tai "siunciam pranesima"
+            // jei patikrinus visus laukus, nerasta bent viena klaida, tai parodome visu klaidos pranesimus (kol kas, viskas pateikiama i console)
+            console.log('All good:', allGood);
+        })
     }
 }
 
